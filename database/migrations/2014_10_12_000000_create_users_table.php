f281f29bd53c526970e400c6abcabdb3870b7c94
a7medelgendy <a7medelgendy@gmail.com>
2023-05-31 23:32:44 +0300

    add required columns for users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('mobile')->unique();
+            $table->boolean('is_approved');
+            $table->enum('gender', ['male', 'female']);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
