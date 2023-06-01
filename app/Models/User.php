69d7530d99d2ed50da5f2dd1e503234798604eb8
a7medelgendy <a7medelgendy@gmail.com>
2023-06-01 03:47:07 +0300

    work on roles and permession using spatie package

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+use Spatie\Permission\Traits\HasPermissions;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles, HasPermissions;
 
     protected $table = 'users'; // set the table name
 
@@ -22,8 +24,9 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'mobile',
+        'gender',
         'password',
-        'is_admin'
     ];
 
     /**
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,16 @@
 <h1>Welcome to the Admin Dashboard</h1>
+@can('manage-customers')
+    <button>manage-customers</button>
+@endcan
+@role('admin')
+    <button type="button">admin only1</button>
+    <button type="button">admin only2</button>
+
+@endrole
+@can('manage-patients')
+    <button type="button">manage-patients</button>
+    <button type="button">manage-appointments</button>
+@endcan
 <form action="{{ route('logout') }}" method="POST">
     @csrf
     <button type="submit">Logout</button>
